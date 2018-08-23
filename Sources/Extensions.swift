72d922f815d3026bac50b7b05ac87518165f2b5d
Duc Do <29248367+pokchy@users.noreply.github.com>
2018-08-23 16:54:05 +0200

    move extension to appropriate file

--- Sources/Extensions.swift
+++ Sources/Extensions.swift
@@ -7,7 +7,6 @@
 //
 
 import Foundation
-import UIKit
 
 internal extension String {
 
@@ -16,22 +15,5 @@ internal extension String {
             return self
         }
         return NSLocalizedString(self, bundle: bundle, comment: self)
-    }
-}
-
-extension UIApplication {
-    class func topViewController(controller: UIViewController? = UIApplication.shared.keyWindow?.rootViewController) -> UIViewController? {
-        if let navigationController = controller as? UINavigationController {
-            return topViewController(controller: navigationController.visibleViewController)
-        }
-        if let tabController = controller as? UITabBarController {
-            if let selected = tabController.selectedViewController {
-                return topViewController(controller: selected)
-            }
-        }
-        if let presented = controller?.presentedViewController {
-            return topViewController(controller: presented)
-        }
-        return controller
     }
 }
