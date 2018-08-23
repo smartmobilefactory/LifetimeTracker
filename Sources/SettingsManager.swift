7677772c4dbfda3c3f86cac3a71b05f309cbd0f7
Duc Do <29248367+pokchy@users.noreply.github.com>
2018-08-23 16:19:47 +0200

    localize strings

--- Sources/SettingsManager.swift
+++ Sources/SettingsManager.swift
@@ -12,18 +12,18 @@ struct SettingsManager {
 
     static func showSettingsActionSheet(on viewController: UIViewController?, hideUntilNewIssuesHandler: @escaping () -> (), hideAlwaysHandler: @escaping () -> ()) {
         let alert = UIAlertController(title: nil, message: nil, preferredStyle: .actionSheet)
-        alert.addAction(UIAlertAction(title: "Hide options", style: .default, handler: { (action: UIAlertAction) in
+        alert.addAction(UIAlertAction(title: "settings.option.hide".lt_localized, style: .default, handler: { (action: UIAlertAction) in
             let alert = UIAlertController(title: nil, message: nil, preferredStyle: .actionSheet)
-            alert.addAction(UIAlertAction(title: "Hide until new issues are detected", style: .default, handler: { (action: UIAlertAction) in
+            alert.addAction(UIAlertAction(title: "settings.option.hide.untilNew".lt_localized, style: .default, handler: { (action: UIAlertAction) in
                 hideUntilNewIssuesHandler()
             }))
-            alert.addAction(UIAlertAction(title: "Hide always", style: .default, handler: { (action: UIAlertAction) in
+            alert.addAction(UIAlertAction(title: "settings.option.hide.always".lt_localized, style: .default, handler: { (action: UIAlertAction) in
                 hideAlwaysHandler()
             }))
-            alert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+            alert.addAction(UIAlertAction(title: "settings.cancel".lt_localized, style: .cancel, handler: nil))
             viewController?.present(alert, animated: true, completion: nil)
         }))
-        alert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+        alert.addAction(UIAlertAction(title: "settings.cancel".lt_localized, style: .cancel, handler: nil))
         viewController?.present(alert, animated: true, completion: nil)
     }
 }
