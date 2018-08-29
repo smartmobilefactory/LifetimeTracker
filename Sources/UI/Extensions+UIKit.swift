fcdab19ee2432271a1330b7807a70a938f442cd1
Duc Do <29248367+pokchy@users.noreply.github.com>
2018-08-29 15:41:26 +0200

    delete unused extension

--- Sources/UI/Extensions+UIKit.swift
+++ Sources/UI/Extensions+UIKit.swift
@@ -15,23 +15,6 @@ internal extension UIView {
     }
 }
 
-internal extension UIApplication {
-    static func topViewController(controller: UIViewController? = UIApplication.shared.keyWindow?.rootViewController) -> UIViewController? {
-        if let navigationController = controller as? UINavigationController {
-            return topViewController(controller: navigationController.visibleViewController)
-        }
-        if let tabController = controller as? UITabBarController {
-            if let selected = tabController.selectedViewController {
-                return topViewController(controller: selected)
-            }
-        }
-        if let presented = controller?.presentedViewController {
-            return topViewController(controller: presented)
-        }
-        return controller
-    }
-}
-
 internal extension UIViewController {
 
     enum HideOption {
