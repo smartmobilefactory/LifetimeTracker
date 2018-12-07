67465cdd76d69dd2690f15666a31446389df36ad
Hans Seiffert <hans.seiffert@smartmobilefactory.com>
2018-12-07 14:22:43 +0100

    Add option in the UI to hide LifetimeTracker

---
+++ Sources/SettingsManager.swift
@@ -0,0 +1,36 @@
+//
+//  SettingsManager.swift
+//  LifetimeTracker
+//
+//  Created by Thanh Duc Do on 23.08.18.
+//  Copyright © 2018 LifetimeTracker. All rights reserved.
+//
+
+import UIKit
+
+struct SettingsManager {
+
+	static func showSettingsActionSheet(on viewController: UIViewController, completionHandler: @escaping (HideOption) -> Void) {
+        let alert = UIAlertController(title: "settings".lt_localized, message: nil, preferredStyle: .actionSheet)
+        alert.addAction(UIAlertAction(title: "settings.option.hide".lt_localized, style: .default, handler: { (action: UIAlertAction) in
+            let alert = UIAlertController(title: "settings.option.hide".lt_localized, message: nil, preferredStyle: .actionSheet)
+            alert.addAction(UIAlertAction(title: "settings.option.hide.untilMoreIssue".lt_localized, style: .default, handler: { (action: UIAlertAction) in
+                completionHandler(.untilMoreIssue)
+            }))
+            alert.addAction(UIAlertAction(title: "settings.option.hide.untilNewType".lt_localized, style: .default, handler: { (action: UIAlertAction) in
+                completionHandler(.untilNewIssueType)
+            }))
+            alert.addAction(UIAlertAction(title: "settings.option.hide.untilRestart".lt_localized, style: .default, handler: { (action: UIAlertAction) in
+                completionHandler(.always)
+            }))
+            alert.addAction(UIAlertAction(title: "settings.cancel".lt_localized, style: .cancel, handler: { (action: UIAlertAction) in
+                completionHandler(.none)
+            }))
+            viewController.present(alert, animated: true, completion: nil)
+        }))
+        alert.addAction(UIAlertAction(title: "settings.cancel".lt_localized, style: .cancel, handler: { (action: UIAlertAction) in
+            completionHandler(.none)
+        }))
+        viewController.present(alert, animated: true, completion: nil)
+    }
+}
