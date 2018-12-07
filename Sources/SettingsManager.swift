f5cf8c5061e0823c771470a12f7350ce530a78f8
Hans Seiffert <hans.seiffert@smartmobilefactory.com>
2018-12-07 14:28:37 +0100

    Improve hide option strings

--- Sources/SettingsManager.swift
+++ Sources/SettingsManager.swift
@@ -12,15 +12,15 @@ struct SettingsManager {
 
 	static func showSettingsActionSheet(on viewController: UIViewController, completionHandler: @escaping (HideOption) -> Void) {
         let alert = UIAlertController(title: "settings".lt_localized, message: nil, preferredStyle: .actionSheet)
-        alert.addAction(UIAlertAction(title: "settings.option.hide".lt_localized, style: .default, handler: { (action: UIAlertAction) in
-            let alert = UIAlertController(title: "settings.option.hide".lt_localized, message: nil, preferredStyle: .actionSheet)
-            alert.addAction(UIAlertAction(title: "settings.option.hide.untilMoreIssue".lt_localized, style: .default, handler: { (action: UIAlertAction) in
+        alert.addAction(UIAlertAction(title: "settings.hide.title".lt_localized, style: .default, handler: { (action: UIAlertAction) in
+            let alert = UIAlertController(title: "settings.hide.sheet.title".lt_localized, message: nil, preferredStyle: .actionSheet)
+            alert.addAction(UIAlertAction(title: "settings.hide.sheet.untilMoreIssue".lt_localized, style: .default, handler: { (action: UIAlertAction) in
                 completionHandler(.untilMoreIssue)
             }))
-            alert.addAction(UIAlertAction(title: "settings.option.hide.untilNewType".lt_localized, style: .default, handler: { (action: UIAlertAction) in
+            alert.addAction(UIAlertAction(title: "settings.hide.sheet.untilNewType".lt_localized, style: .default, handler: { (action: UIAlertAction) in
                 completionHandler(.untilNewIssueType)
             }))
-            alert.addAction(UIAlertAction(title: "settings.option.hide.untilRestart".lt_localized, style: .default, handler: { (action: UIAlertAction) in
+            alert.addAction(UIAlertAction(title: "settings.hide.sheet.untilRestart".lt_localized, style: .default, handler: { (action: UIAlertAction) in
                 completionHandler(.always)
             }))
             alert.addAction(UIAlertAction(title: "settings.cancel".lt_localized, style: .cancel, handler: { (action: UIAlertAction) in
